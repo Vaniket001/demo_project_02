d21a702f565073b727aba480f8316ca14d6b9cc6
Aniket <kumaraniket900@gmail.com>
2022-05-09 07:02:56 +0530

    demo-02 initial commit

---
+++ src/main/java/com/demo/DemoProject02Application.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject02Application.class, args);
+		System.out.println("DemoProject02Application Has Started !!");
+	}
+
+}
